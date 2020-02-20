7dde87d1004c62252c98f3e674f93b71dae103d9
alsebr <alsebrnew@gmail.com>
2020-02-20 11:13:35 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$auth=false;
+
+if(isset($_COOKIE['debug_code'])) {
+    if ( $_COOKIE['debug_code'] = 'a783237fhgjeifG'){
+echo "flag5_********";
+}
+}
+else
+{
+echo "Доступ запрещен <!-- Powered by AwesomeCMS2020 -->";
+};
+;
+
+
+?>
